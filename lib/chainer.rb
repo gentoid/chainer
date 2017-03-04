9ea69153a247eb3f65683d551e1d2a8e47bff307
Viktor Lazarev <taurus101v@gmail.com>
2017-03-04 17:11:49 +0400

    Add missing docs

--- lib/chainer.rb
+++ lib/chainer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Allows to pipe Ruby calls in a nice way
 module Chainer
   autoload :VERSION, 'chainer/version'
 end
--- lib/chainer/version.rb
+++ lib/chainer/version.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 module Chainer
+  # Version of the gem
   VERSION = '0.0.0'
 end
